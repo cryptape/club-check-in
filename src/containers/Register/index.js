9f4402a705c6f2a9a723622ff132a9260aa09fed
Wenyue Wang <realwwy@cryptape.com>
2018-11-01 16:04:01 +0800

    finish register page

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,18 +1,66 @@
 import React from 'react'
 import {observer, inject} from 'mobx-react'
-import {Button} from 'antd-mobile'
+import {Header} from '../../components'
+import {
+    Button,
+    Flex,
+    InputItem,
+    ImagePicker,
+    WhiteSpace,
+} from 'antd-mobile'
+import './register.css'
 
 @inject('registerStore') @observer
 class Register extends React.Component {
 
-    handleRegister = () => {
-        this.props.registerStore.handleHello()
-    }
-
     render() {
+        const {
+            files,
+            onChange,
+            handleRegister,
+        } = this.props.registerStore
+
         return (
-            <div>Register page
-                <Button type={"primary"} onClick={this.handleRegister}>Register</Button>
+            <div className='register-container'>
+                <Flex>
+                    <Flex.Item>
+                        <Header titleName='用户设置'/>
+                    </Flex.Item>
+                </Flex>
+                <Flex className='register-title' justify='center'>
+                    秘猿科技社团 DApp
+                </Flex>
+                <Flex justify='center'>
+                    <InputItem
+                        defaultValue='个人钱包地址'
+                        disabled={true}
+                    >注册账号</InputItem>
+                </Flex>
+                <WhiteSpace size='lg'/>
+                <Flex justify='center'>
+                    <InputItem
+                        placeholder="请输入注册昵称"
+                    >注册昵称</InputItem>
+                </Flex>
+                <Flex className='register-img-title' justify='center'>
+                    选择头像
+                </Flex>
+                <Flex>
+                    <Flex.Item>
+                        <div className='register-img'>
+                            <ImagePicker
+                                files={files}
+                                length={2}
+                                onChange={onChange}
+                                selectable={files.length === 0}
+                            />
+                        </div>
+                    </Flex.Item>
+                </Flex>
+                <WhiteSpace size='lg'/>
+                <Flex justify='center'>
+                    <Button inline type={"primary"} onClick={handleRegister}>完成</Button>
+                </Flex>
             </div>
         )
     }
--- src/stores/registerStore/index.js
+++ src/stores/registerStore/index.js
@@ -1,14 +1,24 @@
 import { observable, action } from 'mobx'
 
+// 后台需要给的数据格式
+// const data = [{
+//     url: 'http://ww1.sinaimg.cn/large/d8eb23c4ly1fwsljvzfu2j20sr0srgqb.jpg',
+//     id: '1111',
+// }, ];
+
 class RegisterStore {
-    @observable name
+    @observable files
 
     constructor() {
-        this.name = 'Register button'
+        this.files = []
     }
 
-    @action handleHello = () => {
-        console.log('hello', this.name)
+    @action onChange = (files) => {
+        this.files = files
+    }
+
+    @action handleRegister = () => {
+        console.log('register button clicked')
     }
 }
 
