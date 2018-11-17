fc028e46f2908e160c5038634903b4415ed6a52f
Wenyue Wang <realwwy@cryptape.com>
2018-11-17 09:55:52 +0800

    upgrade create-react-app to 2.1.1

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,16 @@
-const rewireMobX = require("react-app-rewire-mobx")
 const { injectBabelPlugin } = require('react-app-rewired')
 
 module.exports = function override(config, env) {
+  //
   config = injectBabelPlugin(
-    ['import', { libraryName : 'antd-mobile', style : 'css' }], // change importing css to less
+    ['import', { libraryName : 'antd-mobile', style : 'css' }],
     config,
-  );
+  )
 
-  return rewireMobX(config, env);
-};
+  config = injectBabelPlugin(
+    ["@babel/plugin-proposal-decorators", { legacy : true }],
+    config,
+  )
+
+  return config
+}
