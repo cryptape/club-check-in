2c90b9be54db3a3fb02c77072d3dedccf6e1399d
Qingyangkong <frankkongnj@yahoo.com>
2018-11-21 11:52:27 +0800

    add new mapping in club contract

--- src/contract/ClubCheckInClubs.sol
+++ src/contract/ClubCheckInClubs.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.4.24;
 contract ClubCheckInClubs {
 
     uint256 public number;
-    mapping(address => uint256) public clubs;
+    mapping(address => bool) public clubs;
+    mapping(uint256 => address) public clubsInfo;
 
     constructor() public{
         number = 10000;
     }
 
     function add(address _club) public {
-        require(clubs[_club] == 0);
+        require(!clubs[_club]);
         number = number + 1;
-        clubs[_club] = number;
+        clubsInfo[number] = _club;
     }
 
 }
